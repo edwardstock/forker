1ab55b552c1f42c3e1493dc3f2913e28a5d8bb9a
Eduard Maximovich <edward.vstock@gmail.com>
2016-12-09 20:13:14 +0300

    creating runtime dir for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,6 +10,10 @@ use edwardstock\forker\log\Logger;
 define('FORKER_RPATH', __DIR__ . '/runtime') or defined('FORKER_RPATH');
 define('ENV_TEST', true);
 
+if (!is_dir(FORKER_RPATH)) {
+    mkdir(FORKER_RPATH, 0775, true);
+}
+
 require(__DIR__ . '/../vendor/autoload.php');
 
 $level = Logger::V_NORMAL;
